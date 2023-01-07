b907ed070835a441dc2d6a6270cfcce2f481f99c
sanctuary <51828398+San-ctuary@users.noreply.github.com>
2023-01-07 11:52:39 +0800

    buggy lab1

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,10 +5,13 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if(n % 2 == 0)
-            return n / 2;
-        else
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
             return 3 * n + 1;
+        } else {
+            return n * 2;
+        }
     }
 
     public static void main(String[] args) {
@@ -21,4 +24,3 @@ public class Collatz {
         System.out.println();
     }
 }
-
